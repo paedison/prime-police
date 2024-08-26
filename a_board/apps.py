71f06b1d6da8f641a68605f8c12454c6674ca867
paedison <paedison@gmail.com>
2024-08-26 23:07:44 +0900

    Fix a_board app verbose_name

--- a_board/apps.py
+++ a_board/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ABoardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'a_board'
-    verbose_name = '게시판'
+    verbose_name = '1_게시판'
